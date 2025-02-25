7e14ab688e4bfbc8302a0f8afb94678c6022f227
Siddhant Patil <sidomatic11@gmail.com>
2025-02-25 23:51:40 +0530

    actions workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/studio-website/', // Base path for GitHub Pages
   plugins: [react(), tailwindcss()],
 })
